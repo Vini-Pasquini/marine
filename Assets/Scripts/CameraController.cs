7ab6c060345330723037ecb9f2b4e3151d01f3de
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-09-01 20:53:09 -0300

    camera movement
    - drag and drop camera movement
    - camera lockable on player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,13 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static TMPro.TMP_Compatibility;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject playerBoat;
+    private bool lockOnPlayer;
+
+    private bool movingCamera;
+    private Vector3 clickPosition;
+    private Vector3 anchorPosition;
+    private float cameraSpeed = .2f;
+
+
+    private void Start()
+    {
+        lockOnPlayer = true;
+        clickPosition = Vector3.zero;
+    }
 
     private void Update()
     {
-        this.transform.position = playerBoat.transform.position;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            lockOnPlayer = !lockOnPlayer;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            movingCamera = true;
+            clickPosition = Input.mousePosition;
+            anchorPosition = this.transform.position;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            movingCamera = false;
+            anchorPosition.y = 0f;
+        }
+
+        if (!lockOnPlayer)
+        {
+            if (movingCamera)
+            {
+                Vector3 cameraMovementDelta = clickPosition - Input.mousePosition;
+                Vector3 currentPosition = this.transform.position;
+                currentPosition.x = (cameraMovementDelta.x + cameraMovementDelta.y) * cameraSpeed;
+                currentPosition.y = 0f;
+                currentPosition.z = (cameraMovementDelta.y - cameraMovementDelta.x) * cameraSpeed;
+                this.transform.localPosition = anchorPosition + currentPosition;
+            }
+        }
+        else
+        {
+            this.transform.position = playerBoat.transform.position;
+        }
     }
 }

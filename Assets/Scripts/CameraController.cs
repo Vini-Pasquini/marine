f401ee6defb6abe021805d3fed9b0a75fa2a15af
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-09-04 22:11:22 -0300

    boat outside camera locator [unfinished]
    + fixed rotation
    + added screen rim position
    ! locator flips up when boat gets behind camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,6 +17,10 @@ public class CameraController : MonoBehaviour
     [SerializeField] private GameObject boatLocator;
     private Image boatLocatorImage;
 
+    // LocateBoat()
+    Vector3 newBoatLocatorViewportPosition = Vector3.zero;
+    Vector3 screenCenter;
+    Vector3 screenUp;
 
     private void Start()
     {
@@ -24,6 +28,8 @@ public class CameraController : MonoBehaviour
         clickPosition = Vector3.zero;
         boatLocatorImage = boatLocator.GetComponent<Image>();
         boatLocatorImage.enabled = false;
+        screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(.5f, .5f, 0f)); // const
+        screenUp = Camera.main.ViewportToScreenPoint(new Vector3(0f, .5f, 0f)); // const
     }
 
     private void Update()
@@ -67,17 +73,36 @@ public class CameraController : MonoBehaviour
 
     private void LocateBoat()
     {
+        // test locator flip fix
+        /*
+        Vector3 treco = playerBoat.transform.position - Camera.main.transform.position;
+        float A = Camera.main.transform.forward.x * (Camera.main.transform.up.x - treco.x);
+        float B = Camera.main.transform.forward.y * (Camera.main.transform.up.y - treco.y);
+        float C = Camera.main.transform.forward.y * (Camera.main.transform.up.z - treco.z);
+        if (A + B + C > 0)
+        {
+            Debug.Log("Has Flip");
+        }
+        */
+
         Vector3 boatViewportPosition = Camera.main.WorldToViewportPoint(playerBoat.transform.position);
-        boatViewportPosition.z = 0f;
-        float rotationAngle = Vector3.Angle(Vector3.up, boatViewportPosition - (Vector3.one * .5f));
+        Vector3 boatScreenPosition = Camera.main.WorldToScreenPoint(playerBoat.transform.position);
+        // placeholder - not 100% accurate
+        newBoatLocatorViewportPosition.x = Mathf.Min(1f, Mathf.Max(0f, boatViewportPosition.x));
+        newBoatLocatorViewportPosition.y = Mathf.Min(1f, Mathf.Max(0f, boatViewportPosition.y));
+        boatLocator.transform.position = Camera.main.ViewportToScreenPoint(newBoatLocatorViewportPosition);
+
         if (boatViewportPosition.x <= 0f || boatViewportPosition.x >= 1f || boatViewportPosition.y <= 0f || boatViewportPosition.y >= 1f)
         {
             boatLocatorImage.enabled = true;
-            boatLocator.transform.rotation = Quaternion.Euler(0f, 0f, rotationAngle * (boatViewportPosition.x > .5f ? -1 : 1));
+            boatLocator.transform.rotation = Quaternion.Euler(0f, 0f, (Vector3.Angle(screenUp, boatScreenPosition - screenCenter)) * (boatViewportPosition.x > .5f ? -1 : 1));
         }
         else
         {
             boatLocatorImage.enabled = false;
         }
+
+        Debug.DrawLine(screenCenter, boatScreenPosition);
+        Debug.DrawLine(playerBoat.transform.position, boatScreenPosition);
     }
 }

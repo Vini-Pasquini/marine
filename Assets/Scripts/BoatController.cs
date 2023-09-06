16175ac0e7992a02016e47eec03b98836e5484ba
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-09-06 20:55:08 -0300

    player movement
    + boat stops on target
    + marker disappears on boat arrive
    ! ajust stop speed

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -12,19 +12,15 @@ public class BoatController : MonoBehaviour
     private float boatSpeed = 5f;
     private float rotationSpeed = 2.5f;
 
-    private GameObject clickMarker;
-
-    [SerializeField] private Material MarkerMat;
+    [SerializeField] private GameObject clickMarker;
+    private MeshRenderer clickMarkerRenderer;
 
     private void Start()
     {
         playerBody = this.GetComponent<Rigidbody>();
         movementDirection = Vector3.zero;
         clickPosition = Vector3.zero;
-        clickMarker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        clickMarker.GetComponent<SphereCollider>().enabled = false;
-        clickMarker.GetComponent<MeshRenderer>().material = MarkerMat;
-        clickMarker.SetActive(false);
+        clickMarkerRenderer = clickMarker.GetComponent<MeshRenderer>();
     }
 
     private void Update()
@@ -36,26 +32,28 @@ public class BoatController : MonoBehaviour
             {
                 clickPosition = hitInfo.point;
                 clickMarker.transform.position = clickPosition;
-                if (!clickMarker.activeSelf) clickMarker.SetActive(true);
+                if (!clickMarkerRenderer.enabled) clickMarkerRenderer.enabled = true;
             }
         }
 
         clickMarker.transform.Rotate(Vector3.up, 360 * Time.deltaTime);
-        if (clickMarker.activeSelf && this.transform.position == clickPosition) clickMarker.SetActive(true);
+        if (clickMarkerRenderer.enabled && playerBody.velocity.magnitude == 0f) clickMarkerRenderer.enabled = false;
 
         movementDirection = (clickPosition - this.transform.position);
     }
 
     private void FixedUpdate()
     {
-        if (movementDirection.magnitude > 1f)
-        {
-            float currentAngle = Vector3.Angle(this.transform.forward, movementDirection);
-            playerBody.velocity = this.transform.forward * (boatSpeed * (currentAngle >= 90 ? .3f : (currentAngle >= 45 ? .6f : 1f)));
-            if (currentAngle > 1f)
-            {
-                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(movementDirection), Time.deltaTime * rotationSpeed);
-            }
-        }
+        MovePlayerBoat();
+    }
+
+    private void MovePlayerBoat()
+    {
+        float speedMultiplier = Mathf.Min(movementDirection.magnitude, 1f);
+        if (movementDirection.magnitude <= .5f) speedMultiplier = 0f;
+        float currentAngle = Vector3.Angle(this.transform.forward, movementDirection);
+        Vector3 currentVelocity = this.transform.forward * (boatSpeed * (currentAngle >= 90 ? .3f : (currentAngle >= 45 ? .6f : 1f)));
+        if (currentAngle > 5f && speedMultiplier > 0f) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(movementDirection), Time.deltaTime * rotationSpeed);
+        playerBody.velocity = currentVelocity * speedMultiplier;
     }
 }

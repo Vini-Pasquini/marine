4c603249841e1381c2298b9e7b397a40e4c60141
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-09-11 19:09:27 -0300

    set resolution on game start

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            Screen.SetResolution(1600, 900, false);
+        }
+    }
+
     public void OnChangeSceneButtonPress(string newScene)
     {
         SceneManager.LoadScene(newScene);

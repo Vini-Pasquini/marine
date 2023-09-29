56db3111f8600e41a5649c83662e56538496d6b9
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-09-29 18:39:43 -0300

    level state + qof
    + added support for different level states
    ! added lower bound to health, fuel and gold in each Increment methods
    ! playerLoadStaged get and set name change for better clarity

--- Assets/Scripts/Core.cs
+++ Assets/Scripts/Core.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public static class Core
 {
     // levelState
+    private static LEVEL_STATE currentLevelState = LEVEL_STATE.Stopped;
+    public static LEVEL_STATE GetLevelState() { return currentLevelState; }
+    public static void SetLevelState(LEVEL_STATE newState) { currentLevelState = newState; }
+
     private static float playerFuel = 0f;
     public static float GetPlayerFuel() { return playerFuel; }
     public static void SetPlayerFuel(float newValue) { playerFuel = newValue; }
-    public static void IncrementPlayerFuel(float increment) { playerFuel += increment; }
+    public static void IncrementPlayerFuel(float increment) { playerFuel += increment; if (playerFuel < 0f) playerFuel = 0f; }
 
     private static float playerHealth = 0f;
     public static float GetPlayerHealth() { return playerHealth; }
     public static void SetPlayerHealth(float newValue) { playerHealth = newValue; }
-    public static void IncrementPlayerHealth(float increment) { playerHealth += increment; }
+    public static void IncrementPlayerHealth(float increment) { playerHealth += increment; if (playerHealth < 0f) playerHealth = 0f; }
 
     private static float playerGold = 0f;
     public static float GetPlayerGold() { return playerGold; }
     public static void SetPlayerGold(float newValue) { playerGold = newValue; }
-    public static void IncrementPlayerGold(float increment) { playerGold += increment; }
+    public static void IncrementPlayerGold(float increment) { playerGold += increment; if (playerGold < 0f) playerGold = 0f; }
 
     private static Vector3 playerPosition = Vector3.zero;
     public static Vector3 GetPlayerPosition() { return playerPosition; }
@@ -30,20 +34,21 @@ public static class Core
 
     // flags
     private static bool playerLoadStaged = false;
-    public static bool GetPlayerLoadStaged() { return playerLoadStaged; }
-    public static void SetPlayerLoadStaged(bool newValue) { playerLoadStaged = newValue; }
+    public static bool IsPlayerLoadStaged() { return playerLoadStaged; }
+    public static void StagePlayerLoad(bool newValue) { playerLoadStaged = newValue; }
 
     // gameState
     private static string lastActiveSceneName = null;
     public static string GetLastActiveScene() { return lastActiveSceneName; }
     public static void SetLastActiveScene(string sceneName) { lastActiveSceneName = sceneName; }
 
-    public static void Reset()
+    public static void Reset(LEVEL_STATE levelStateOverride = LEVEL_STATE.Stopped)
     {
         playerFuel = 100f;
         playerHealth = 100f;
         playerGold = 0f;
         playerPosition = Vector3.zero;
         playerRotation = Quaternion.identity;
+        currentLevelState = levelStateOverride;
     }
 }

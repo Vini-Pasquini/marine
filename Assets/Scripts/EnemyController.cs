1d8cbf03a0a69be170fa07efaefe8c5e20e62780
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-10-04 17:36:09 -0300

    enemy
    + placeholder model
    + enemy controller structure
    ! highlight as model mesh

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private void Start()
+    {
+        /* enemy init */
+        // select enemy type ()
+        // load enemy model
+        // apply convex mesh collider
+        /* enemy highlight init */
+        // dup model mesh as enemy highlight
+        // set enemy highlight to "enemy" layer
+        // change material to EnemyHighlightMat
+        // meshrenderer.enabled = false
+    }
+
+    private void Update()
+    {
+        
+    }
+}

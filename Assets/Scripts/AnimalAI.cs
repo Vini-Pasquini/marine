110654608e97392d7f3615ac45f0f8af545ea3bc
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-10-06 18:29:06 -0300

    animal ai movement
    + added better placeholder movement to animal (similar to player boat)

--- Assets/Scripts/AnimalAI.cs
+++ Assets/Scripts/AnimalAI.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class AnimalAI : MonoBehaviour
 {
     GameObject animalWaypoint = null;
-    
+    Rigidbody animalBody;
+
     public void SetWaypointObject(GameObject newWaypoint)
     {
         animalWaypoint = newWaypoint;
+    }
+
+    private void Start()
+    {
+        animalBody = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -19,7 +25,18 @@ public class AnimalAI : MonoBehaviour
     private void FollowWaypoint()
     {
         if (animalWaypoint == null) return;
-        this.transform.position = animalWaypoint.transform.position; // placeholder
+        // this.transform.position = animalWaypoint.transform.position; // placeholder
+
+        Vector3 movementDirection = animalWaypoint.transform.position - this.transform.position;
+        float animalSpeed = 15f;
+        float rotationSpeed = 1.5f;
+
+        float currentAngle = Vector3.Angle(this.transform.forward, movementDirection);
+        Vector3 currentVelocity = this.transform.forward * (animalSpeed * (currentAngle >= 90 ? .3f : (currentAngle >= 45 ? .6f : 1f)));
+
+        if (currentAngle > 10f) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(movementDirection), Time.deltaTime * rotationSpeed);
+
+        animalBody.velocity = currentVelocity;
     }
 
 }

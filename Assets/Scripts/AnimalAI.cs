30d5f463d60dd9b36d6b013358f37fe4e1d1dda5
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-10-09 17:18:02 -0300

    animal movement speed
    + added support for variable movement speed

--- Assets/Scripts/AnimalAI.cs
+++ Assets/Scripts/AnimalAI.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class AnimalAI : MonoBehaviour
 {
-    GameObject animalWaypoint = null;
-    Rigidbody animalBody;
+    private GameObject animalWaypoint = null;
+    private Rigidbody animalBody;
+
+    private float animalSpeed = 0f;
+    private float rotationRate = 0f; // always 10% of animalSpeed
 
     public void SetWaypointObject(GameObject newWaypoint)
     {
         animalWaypoint = newWaypoint;
+    }
+
+    public void SetAnimalSpeed(float newSpeed)
+    {
+        animalSpeed = newSpeed;
+    }
+
+    public void SetAnimalRotationRate(float newRate)
+    {
+        rotationRate = newRate;
     }
 
     private void Start()
@@ -27,14 +40,14 @@ public class AnimalAI : MonoBehaviour
         if (animalWaypoint == null) return;
         // this.transform.position = animalWaypoint.transform.position; // placeholder
 
-        Vector3 movementDirection = animalWaypoint.transform.position - this.transform.position;
-        float animalSpeed = 15f;
-        float rotationSpeed = 1.5f;
+        Vector3 waypointPosition = animalWaypoint.transform.position;
+        waypointPosition.y = -1f;
+        Vector3 movementDirection = waypointPosition - this.transform.position;
 
         float currentAngle = Vector3.Angle(this.transform.forward, movementDirection);
         Vector3 currentVelocity = this.transform.forward * (animalSpeed * (currentAngle >= 90 ? .3f : (currentAngle >= 45 ? .6f : 1f)));
 
-        if (currentAngle > 10f) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(movementDirection), Time.deltaTime * rotationSpeed);
+        if (currentAngle > 10f) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(movementDirection), Time.deltaTime * rotationRate);
 
         animalBody.velocity = currentVelocity;
     }

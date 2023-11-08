db81e25bb094672a6c6b5ad8af2826abac5d1d69
Vini-Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2023-11-08 14:49:48 -0300

    level 3 + QR Code
    + placeholder level 3 done
    + added playtest QR Code

--- Assets/Scripts/LevelUnlocker.cs
+++ Assets/Scripts/LevelUnlocker.cs
@@ -7,11 +7,15 @@ public class LevelUnlocker : MonoBehaviour
 {
     [SerializeField] Button[] levelButtons;
 
+    [SerializeField] GameObject QRCode;
+
     private void Start()
     {
         for (int index = Mathf.Min(Core.GetLevelProgress(), levelButtons.Length - 1); index >= 0; index--)
         {
             levelButtons[index].interactable = true;
         }
+
+        if (Core.GetLevelProgress() >= 3) QRCode.SetActive(true);
     }
 }
